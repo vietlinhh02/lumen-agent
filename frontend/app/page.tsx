c3c97cbfade8c10898be72d07c092cdeb0f909a5
VietLinh <nvlinh0607@gmail.com>
2026-06-11 19:56:59 +0700

    feat: register GSAP plugins and add CustomCursor to landing page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+
 import { Navbar } from "@/components/landing/Navbar";
 import { Hero } from "@/components/landing/Hero";
 import { WorkflowSteps } from "@/components/landing/WorkflowSteps";
@@ -5,10 +11,18 @@ import { PainPoints } from "@/components/landing/PainPoints";
 import { Features } from "@/components/landing/Features";
 import { CtaBand } from "@/components/landing/CtaBand";
 import { Footer } from "@/components/landing/Footer";
+import { CustomCursor } from "@/components/landing/CustomCursor";
+
+gsap.registerPlugin(ScrollTrigger, useGSAP);
 
 export default function LandingPage() {
+  useGSAP(() => {
+    ScrollTrigger.refresh();
+  });
+
   return (
     <div className="min-h-screen bg-canvas">
+      <CustomCursor />
       <Navbar />
       <Hero />
       <WorkflowSteps />
